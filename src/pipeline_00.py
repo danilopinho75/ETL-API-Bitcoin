ec7f28a01b62bdbf64a358334bc87fb4f9e821e3
danilopinho75 <danilopinho11@gmail.com>
2024-12-27 12:59:30 -0300

    Transformação dos dados da API

--- src/pipeline_00.py
+++ src/pipeline_00.py
@@ -6,4 +6,22 @@ def extract_data_bitcoin():
     data = response.json()
     return data
 
-print(extract_data_bitcoin()["data"])
+def transform_data_bitcoin(data):
+    valor = data["data"]["amount"]
+    criptomoeda = data["data"]["base"]
+    moeda = data["data"]["currency"]
+
+    data_transform = {
+        "valor": valor,
+        "criptomoeda": criptomoeda,
+        "moeda": moeda
+    }
+
+    return data_transform
+
+
+if __name__ == "__main__":
+    # Extração dos dados
+    data_json = extract_data_bitcoin()
+    data_processed = transform_data_bitcoin(data_json)
+    print(data_processed)
